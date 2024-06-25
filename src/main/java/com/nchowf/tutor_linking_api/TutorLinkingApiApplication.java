638c26d323179569d16d90c06d9b6fa1136d2c3e
chihoa-nguyen <nguyenchihoa.ptithcm.d19@gamil.com>
2024-06-25 14:10:26 +0700

    upload src

---
+++ src/main/java/com/nchowf/tutor_linking_api/TutorLinkingApiApplication.java
@@ -0,0 +1,12 @@
+package com.nchowf.tutor_linking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorLinkingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorLinkingApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nchowf/tutor_linking_api/TutorLinkingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nchowf.tutor_linking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorLinkingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
