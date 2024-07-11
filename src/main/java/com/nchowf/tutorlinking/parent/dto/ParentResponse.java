afe58236892b6aa4be113daa123da7bc4c0d7f56
chihoa-nguyen <nguyenchihoa.ptithcm.d19@gamil.com>
2024-07-11 15:08:58 +0700

    refactor: remove unnecessary constructors in tutor/parent responses

--- src/main/java/com/nchowf/tutorlinking/parent/dto/ParentResponse.java
+++ src/main/java/com/nchowf/tutorlinking/parent/dto/ParentResponse.java
@@ -2,17 +2,11 @@ package com.nchowf.tutorlinking.parent.dto;
 
 import com.nchowf.tutorlinking.user.dto.UserResponse;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
 
-import java.util.Date;
-
-@Data
+@EqualsAndHashCode(callSuper = true)
+@Data()
 @SuperBuilder
 public class ParentResponse extends UserResponse {
-    public ParentResponse(Integer id, String email, String name, String phoneNumber, String address, Date createdAt, Date updatedAt) {
-        super(id, email, name, phoneNumber, address, createdAt, updatedAt);
-    }
-    public ParentResponse() {
-        super();
-    }
 }
--- src/main/java/com/nchowf/tutorlinking/tutor/dto/TutorResponse.java
+++ src/main/java/com/nchowf/tutorlinking/tutor/dto/TutorResponse.java
@@ -4,11 +4,13 @@ import com.nchowf.tutorlinking.grade.dto.GradeResponse;
 import com.nchowf.tutorlinking.subject.dto.SubjectResponse;
 import com.nchowf.tutorlinking.user.dto.UserResponse;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
 
 import java.util.Date;
 import java.util.Set;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @SuperBuilder
 public class TutorResponse extends UserResponse {
@@ -23,20 +25,4 @@ public class TutorResponse extends UserResponse {
     private Set<GradeResponse> grades;
     private Set<String> teachingArea;
     private String description;
-
-    public TutorResponse(Integer id, String email, String name, String phoneNumber, String address, Date createdAt, Date updatedAt, String birthday, String gender, String avt, String degree, String university_name, String major, String position, Set<SubjectResponse> subjects, Set<GradeResponse> grades, Set<String> teachingArea, String description) {
-        super(id, email, name, phoneNumber, address, createdAt, updatedAt);
-        this.birthday = birthday;
-        this.gender = gender;
-        this.avt = avt;
-        this.degree = degree;
-        this.university_name = university_name;
-        this.major = major;
-        this.position = position;
-        this.subjects = subjects;
-        this.grades = grades;
-        this.teachingArea = teachingArea;
-        this.description = description;
-    }
-
 }
